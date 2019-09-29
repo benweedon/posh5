2e9f896add5c487691b10e9e0fc9081e2bebf872
Ben Weedon <ben@weedon.email>
2019-09-29 12:41:21 -0700

    Initial commit

---
+++ Invoke-P5Sketch.ps1
@@ -0,0 +1,73 @@
+﻿function Invoke-P5Sketch {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, Position=0)]
+        [String] $SketchPath,
+        [Parameter(Mandatory=$True, Position=1)]
+        [String] $OutFile,
+        [Parameter(Mandatory=$False)]
+        [UInt32] $Port = 8080,
+        [Parameter(Mandatory=$False)]
+        [UInt32] $Delay = 10000
+    )
+    PROCESS {
+        $temp = CreateTemporaryHtmlFile
+        try {
+            $SketchPath = "file:///$(Resolve-Path $SketchPath)".Replace("\", "\\")
+            ReplaceTemplate $temp $SketchPath $Port $Delay
+
+            # Open the HTML file.
+            Start-Process "file:///$temp"
+
+            $listener = New-Object System.Net.HttpListener
+            try {
+                # Begin listening on the given port.
+                $listener.Prefixes.Add("http://localhost:$Port/")
+                $listener.Start()
+                $context = $listener.GetContext()
+
+                # Read the data URL from the POST message.
+                $dataUrl = (New-Object System.IO.StreamReader $context.Request.InputStream).ReadToEnd()
+
+                # Convert the data URL to bytes and write it out to the file.
+                $bytes = DataUrlToBytes $dataUrl
+                $bytes | Set-Content $OutFile -Encoding Byte
+            } finally {
+                $listener.Stop()
+            }
+        } finally {
+            Remove-Item $temp
+        }
+    }
+}
+
+function CreateTemporaryHtmlFile() {
+    $temp = New-TemporaryFile
+    $tempHtml = "$($temp.FullName).html"
+    Move-Item $temp $tempHtml
+    return $tempHtml
+}
+
+function ReplaceTemplate([String] $TemplateFile, [String] $SketchPath, [UInt32] $Port, [UInt32] $Delay) {
+    $templateContent = Get-Content "$PSScriptRoot\run.html" -Encoding UTF8
+
+    $templateContent = $templateContent.Replace("{ sketchPath }", "'$SketchPath'")
+    $templateContent = $templateContent.Replace("{ port }", $Port)
+    $templateContent = $templateContent.Replace("{ delay }", $Delay)
+
+    $templateContent | Out-File $TemplateFile -Encoding UTF8
+}
+
+function DataUrlToBytes([String] $DataUrl) {
+    $dataUrlHeader = "data:image/png;"
+    if ($DataUrl.StartsWith($dataUrlHeader)) {
+        $DataUrl = $DataUrl.Remove(0, $dataUrlHeader.Length)
+    }
+
+    $base64Header = "base64,"
+    if ($DataUrl.StartsWith($base64Header)) {
+        $DataUrl = $DataUrl.Remove(0, $base64Header.Length)
+    }
+
+    return [Convert]::FromBase64String($DataUrl)
+}
---
+++ posh5.psd1
@@ -0,0 +1,35 @@
+﻿# Module manifest for module "posh5.psm1"
+@{
+
+# Script module or binary module file associated with this manifest
+RootModule = "posh5.psm1"
+
+# Version number of this module.
+ModuleVersion = "0.1"
+
+# ID used to uniquely identify this module
+GUID = "bb395b2f-b569-4b5e-9bd0-5864554ff4f0"
+
+# Author of this module
+Author = "Ben Weedon"
+
+# Description of the functionality provided by this module
+Description = "Command line utilities for p5"
+
+# TODO: Are these minimum versions correct?
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = "5.0"
+
+# Minimum version of the common language runtime (CLR) required by this module
+CLRVersion = "4.0"
+
+# Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+NestedModules = @(
+    "Invoke-P5Sketch.ps1")
+
+# Cmdlets to export from this module
+CmdletsToExport = @(
+    "Invoke-P5Sketch")
+
+AliasesToExport = @()
+}
